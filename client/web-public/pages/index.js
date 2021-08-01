8492062e5a0cd479a6c85235104ef910a90502e3
Maksim Dmitriev <lalalabtv@gmail.com>
2021-08-01 13:31:08 +0300

    Router Fix

--- client/web-public/pages/index.js
+++ client/web-public/pages/index.js
@@ -1,9 +1,16 @@
-
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
       Hello World!
+      <ul>
+        <li>
+          <Link href="/auth">
+            Auth Route
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
