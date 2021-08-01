c419de571bff662aa7fd0b39ec5a7882f7d3cc6d
Pugavkomm <slavapugavko2@gmail.com>
2021-08-01 22:12:26 +0300

    Rename database. Update index.js in web-client. Minor changes

--- client/web-public/pages/index.js
+++ client/web-public/pages/index.js
@@ -3,14 +3,65 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      Hello World!
+      Openschool
       <ul>
         <li>
           <Link href="/auth">
-            Auth Route
+            Auth Route -- страница аутентификации
           </Link>
         </li>
       </ul>
+
+      <ul>
+        <li>
+          <Link href="/admin">
+            web-admin
+          </Link>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <Link href="/processes">
+            web-processes
+          </Link>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <Link href="/api/content">
+            api-content
+          </Link>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <Link href="api/extra">
+            api-extra
+          </Link>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <Link href="api/stuff">
+            api-stuff
+          </Link>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <Link href="api/users">
+            api-users
+          </Link>
+        </li>
+      </ul>
+
     </div>
+
+    
   )
 }
